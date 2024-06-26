515c10c9d1c95ee8e7267b63b7720c733bdef425
Sokkowastaken <xjoostx@hotmail.nl>
2024-06-26 02:02:22 +0200

    removing Code from repo

--- Code - OSS/User/History/-52dddc7a/WMif.lua
+++
@@ -1,9 +0,0 @@
-return{
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-        local latte = require("catppuccin.palettes").get_palette "latte"
-        vim.cmd.colorscheme "catppuccin"
-    end
-}
